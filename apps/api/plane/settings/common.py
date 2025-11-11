6b160d980f206cc4cc62ceffe85064adb3d11712
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2025-11-11 19:35:02 +0530

    feat: adding outbox poller app

--- apps/api/plane/settings/common.py
+++ apps/api/plane/settings/common.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "plane.license",
     "plane.api",
     "plane.authentication",
+    "plane.event_stream",
     # Third-party things
     "rest_framework",
     "corsheaders",
